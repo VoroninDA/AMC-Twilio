50286128d7d93b97fe4b03b605bce066dc9b38aa
Dmitriy A. Voronin <dvoronin@amctechnology.com>
2018-02-14 09:45:12 -0500

    Demoable

--- public/callcenter/AMC/TwilioPlugin.js
+++ public/callcenter/AMC/TwilioPlugin.js
@@ -37,7 +37,7 @@ window.addEventListener('newWorker', function (newWorker){
     AMCWorkerJS.on('reservation.created', function (reservation) {
 
         console.log("Found a reservation: " + reservation)
-        alert(reservation.task.attributes.caller);
+        //alert(reservation.task.attributes.caller);
         //reservation.task.attributes
         //reservation.task.attributes.caller THIS IS THE PHONE NUMBER
     });

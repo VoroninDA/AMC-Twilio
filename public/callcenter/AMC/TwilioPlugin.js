41ffa08bb700c7728203c3ce6ddf360dd18343d0
Dmitriy A. Voronin <dvoronin@amctechnology.com>
2018-02-16 16:53:30 -0500

    ScreenPOP

--- public/callcenter/AMC/TwilioPlugin.js
+++ public/callcenter/AMC/TwilioPlugin.js
@@ -5,9 +5,8 @@ $(document).ready(function () {
     var Config = {};
     var localStorage = window.localStorage;
     var inpHost = window.location.origin;
-    var myIntercationID = 0;
     var myInteractionID = 0;
-    var scenarioId = 0;
+    var myScenarioId = 0;
 
     ContactCanvas.Channel.enableClickToDial(ContactCanvas.Commons.getSequenceID(), function () { console.log("Click to Dial Enabled"); });
     //ContactCanvas.Channel.registerClickToDialEvent(ContactCanvas.Commons.getSequenceID(), clickToDialCallback);
@@ -41,7 +40,7 @@ $(document).ready(function () {
 
         AMCWorkerJS.on('reservation.created', function (reservation) {
             myInteractionID = ContactCanvas.Commons.getSequenceID();
-            scenarioId = ContactCanvas.Commons.getSequenceID();
+            myScenarioId = ContactCanvas.Commons.getSequenceID();
             var interactionState = ContactCanvas.Commons.interactionStates.Alerting;
             var details = new ContactCanvasChannelAPI.RecordItem("", "", "");
             var interactionType = '';
@@ -65,112 +64,126 @@ $(document).ready(function () {
                 state: interactionState,
                 details: details,
                 interactionId: myInteractionID,
-                scenarioId: myInteractionID,
+                scenarioId: myScenarioId,
                 interactionDirection: interactionDirection
             });
-            myInteractionID++;
-/*
-           
-        $scope.workerJS.on('reservation.accepted', function (reservation) {
-    
-            $log.log('TaskRouter Worker: reservation.accepted');
-            $log.log(reservation);
-    
-            $scope.task = reservation.task;
-    
-            /* check if the customer name is a phone number *//*
-        var pattern = /(.*)(\+[0-9]{8,20})(.*)$/;
+        });
 
-        if (pattern.test($scope.task.attributes.name) === true) {
-            $scope.task.attributes.nameIsPhoneNumber = true;
-        }
+        AMCWorkerJS.on('reservation.timeout', function (reservation) {
 
-        $scope.task.completed = false;
-        $scope.reservation = null;
-        $scope.stopReservationCounter();
+            var direction = ContactCanvas.Commons.InteractionDirectionTypes.Inbound; //changed to inbound as Screenpop not happening for outbound.Ben to check Code.
+            var state = ContactCanvas.Commons.interactionStates.Disconnected;
+            ContactCanvas.Channel.setInteraction(ContactCanvas.Commons.getSequenceID(), {
+                state: state,
+                details: null,
+                interactionId: myInteractionID,
+                interactionDirection: direction,
+                scenarioId: myScenarioId
+            });
+            myInteractionID = 0;
+            myScenarioId = 0;
 
-        $scope.$apply();
+        });
 
-    });
+        AMCWorkerJS.on('reservation.rescinded', function (reservation) {
+
+            var direction = ContactCanvas.Commons.InteractionDirectionTypes.Inbound; //changed to inbound as Screenpop not happening for outbound.Ben to check Code.
+            var state = ContactCanvas.Commons.interactionStates.Disconnected;
+            ContactCanvas.Channel.setInteraction(ContactCanvas.Commons.getSequenceID(), {
+                state: state,
+                details: null,
+                interactionId: myInteractionID,
+                interactionDirection: direction,
+                scenarioId: myScenarioId
+            });
+            myInteractionID = 0;
+            myScenarioId = 0;
+
+        });
+
+        AMCWorkerJS.on('reservation.canceled', function (reservation) {
+
+            var direction = ContactCanvas.Commons.InteractionDirectionTypes.Inbound; //changed to inbound as Screenpop not happening for outbound.Ben to check Code.
+            var state = ContactCanvas.Commons.interactionStates.Disconnected;
+            ContactCanvas.Channel.setInteraction(ContactCanvas.Commons.getSequenceID(), {
+                state: state,
+                details: null,
+                interactionId: myInteractionID,
+                interactionDirection: direction,
+                scenarioId: myScenarioId
+            });
+            myInteractionID = 0;
+            myScenarioId = 0;
+
+        });
+        /*
+               
+            $scope.workerJS.on('reservation.accepted', function (reservation) {
+        
+                $log.log('TaskRouter Worker: reservation.accepted');
+                $log.log(reservation);
+        
+                $scope.task = reservation.task;
+        
+                /* check if the customer name is a phone number *//*
+    var pattern = /(.*)(\+[0-9]{8,20})(.*)$/;
+
+    if (pattern.test($scope.task.attributes.name) === true) {
+        $scope.task.attributes.nameIsPhoneNumber = true;
+    }
+
+    $scope.task.completed = false;
+    $scope.reservation = null;
+    $scope.stopReservationCounter();
+
+    $scope.$apply();
+
+});
 */
-    $scope.workerJS.on('reservation.timeout', function (reservation) {
-
-        $log.log('TaskRouter Worker: reservation.timeout');
-        $log.log(reservation);
-
-        /* reset all data */
-        $scope.reservation = null;
-        $scope.task = null;
-        $scope.$apply();
-
-    });
-
-    $scope.workerJS.on('reservation.rescinded', function (reservation) {
-
-        $log.log('TaskRouter Worker: reservation.rescinded');
-        $log.log(reservation);
-
-        /* reset all data */
-        $scope.reservation = null;
-        $scope.task = null;
-        $scope.$apply();
-
-    });
-
-    $scope.workerJS.on('reservation.canceled', function (reservation) {
-
-        $log.log('TaskRouter Worker: reservation.cancelled');
-        $log.log(reservation);
-
-        $scope.reservation = null;
-        $scope.task = null;
-        $scope.$apply();
-
-    });
-/*
-    $scope.workerJS.on('activity.update', function (worker) {
-
-        $log.log('TaskRouter Worker: activity.update');
-        $log.log(worker);
-
-        $scope.worker = worker;
-        $scope.$apply();
-
-    });
-
-    $scope.workerJS.on('token.expired', function () {
-
-        $log.log('TaskRouter Worker: token.expired');
-
-        $scope.reservation = null;
-        $scope.task = null;
-        $scope.$apply();
-
-        /* the worker token expired, the agent shoud log in again, token is generated upon log in *//*
+        /*
+            $scope.workerJS.on('activity.update', function (worker) {
+        
+                $log.log('TaskRouter Worker: activity.update');
+                $log.log(worker);
+        
+                $scope.worker = worker;
+                $scope.$apply();
+        
+            });
+        
+            $scope.workerJS.on('token.expired', function () {
+        
+                $log.log('TaskRouter Worker: token.expired');
+        
+                $scope.reservation = null;
+                $scope.task = null;
+                $scope.$apply();
+        
+                /* the worker token expired, the agent shoud log in again, token is generated upon log in *//*
         window.location.replace('/callcenter/');
 
     });
 
     /* the agent's browser lost the connection to Twilio *//*
-        $scope.workerJS.on('connected', function () {
-            $log.log('TaskRouter Worker: WebSocket has connected');
-            $scope.UI.warning.worker = null;
-            $scope.$apply();
-        });
-    
-        $scope.workerJS.on('disconnected', function () {
-            $log.error('TaskRouter Worker: WebSocket has disconnected');
-            $scope.UI.warning.worker = 'TaskRouter Worker: WebSocket has disconnected';
-            $scope.$apply();
-        });
-    
-        $scope.workerJS.on('error', function (error) {
-            $log.error('TaskRouter Worker: an error occurred: ' + error.response + ' with message: ' + error.message);
-            $scope.UI.warning.worker = 'TaskRouter Worker: an error occured: ' + error.response + ' with message: ' + error.message;
-            $scope.$apply();
-        });
-    
-    };*/
+            $scope.workerJS.on('connected', function () {
+                $log.log('TaskRouter Worker: WebSocket has connected');
+                $scope.UI.warning.worker = null;
+                $scope.$apply();
+            });
+        
+            $scope.workerJS.on('disconnected', function () {
+                $log.error('TaskRouter Worker: WebSocket has disconnected');
+                $scope.UI.warning.worker = 'TaskRouter Worker: WebSocket has disconnected';
+                $scope.$apply();
+            });
+        
+            $scope.workerJS.on('error', function (error) {
+                $log.error('TaskRouter Worker: an error occurred: ' + error.response + ' with message: ' + error.message);
+                $scope.UI.warning.worker = 'TaskRouter Worker: an error occured: ' + error.response + ' with message: ' + error.message;
+                $scope.$apply();
+            });
+        
+        };*/
 
     });
     //for interaction state changes, please see the workflow and phone controller.
@@ -178,7 +191,7 @@ $(document).ready(function () {
     setHeightOfSoftphone();
 
 });
-function AMCdisconnect(){
+function AMCdisconnect() {
 
     //var details = new ContactCanvas.Commons.RecordItem("", "", "");
     //details.setPhone("", "", 6464023580);

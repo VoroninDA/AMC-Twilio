44433c6231672f8f22a92e70f71c44f1691f206c
Dmitriy A. Voronin <dvoronin@amctechnology.com>
2018-02-19 12:43:06 -0500

    ScreenPOP

--- public/callcenter/AMC/TwilioPlugin.js
+++ public/callcenter/AMC/TwilioPlugin.js
@@ -201,7 +201,46 @@ window.location.replace('/callcenter/');
         });
     
     }
+    function setHeightOfSoftphone()
+{
+    var heightObj = getHeight();
+    
+    ContactCanvas.Channel.setSoftphoneHeight(ContactCanvas.Commons.getSequenceID(),heightObj, null);
+}
 
+/*function getHeight()
+{
+    //var profile = getLocalStorageProfile();
+    height = 300;
+
+    /*if(profile.phoneNumber)
+    {
+        var phoneData = getPhoneNumberData(profile.phoneNumber);
+       
+        height += 130;
+        height +=  getCadHeight(phoneData);
+    }
+    if(profile.outPhoneNumber)
+    {
+        height+= 130;
+    }
+
+    if(profile.warmTransferCallID)
+    {
+        height+= 130;
+    }
+    if(profile.conferenceCallID && !profile.conferenceCallCompleted)
+    {
+        height+= 130;
+    }
+    else if(profile.conferenceCallID && profile.conferenceCallCompleted)
+    {
+        height += 210;
+    }
+    
+    currentHeight = height;
+    return { height : height};
+}*/
 });
 /*
 function updateInteractionAndScenarioID (scenario, interaction){

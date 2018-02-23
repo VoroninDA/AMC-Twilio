08906835e83e9272a052ade934faa361c931ba46
Dmitriy A. Voronin <dvoronin@amctechnology.com>
2018-02-23 15:27:50 -0500

    Presence

--- public/callcenter/AMC/login.js
+++ public/callcenter/AMC/login.js
@@ -2,5 +2,9 @@ $(document).ready(function () {
     ContactCanvas.Channel.registerForLoginEvents(ContactCanvas.Commons.getSequenceID(), function(){
         document.getElementById("loginform").style.display = "block";
     });
+    ContactCanvas.Channel.registerContextualControls(ContactCanvas.Commons.getSequenceID(), null);
+    var iconPath = {};
+    iconPath.pluginIconPath = window.location.origin + "/callcenter/AMC/img/twilio.png";
+    ContactCanvas.Channel.addPluginImage(ContactCanvas.Commons.getSequenceID(), iconPath, null);
     ContactCanvas.Channel.initializationComplete(ContactCanvas.Commons.getSequenceID(), {}, function(){});
 });
